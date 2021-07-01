c7227753152fdf5a9e69ebb1ca2e569492174bb5
saur0x <>
2021-07-01 16:16:32 +0530

    Made the library header only. Improved test.c example.

--- tests/test.c
+++ tests/test.c
@@ -5,6 +5,10 @@
 #include "midi_parser.h"
 
 
+#define REAL_TIME
+#define SHOW_KEYBOARD
+
+
 void show_keyboard(uint8_t *notes, size_t size, FILE *output)
 {
 	for (size_t i = 21; i <= 108; ++i) {
@@ -13,17 +17,16 @@ void show_keyboard(uint8_t *notes, size_t size, FILE *output)
 	putc('\n', output);
 }
 
-void parse_midi(struct midi_parser *parser, FILE *midi)
+void parse(FILE *midi)
 {
+	struct midi_parser *parser = midi_parser_new(NULL, midi);
 	struct midi_event event;
+    uint8_t note, event_on, notes[128] = { 0 };
+	FILE *data_stream = stdout;
 
-	uint8_t notes[128] = { 0 };
-	uint8_t note, event_on;
-
-	while (!parser->end_of_file) {
+	for (; !parser->end_of_file; parser->timestamp += parser->dtime) {
 		midi_parser_next(parser, midi, &event);
 
-		// switch (event.status & 0xF0) {
 		switch (MIDI_EVENT_TYPE(&event)) {
 			case EventNoteOn: {
 				note = event.midi_data[0];
@@ -37,19 +40,46 @@ void parse_midi(struct midi_parser *parser, FILE *midi)
 				notes[note] = event_on;
 				break;
 			}
+			case EventSystemExclusive: {
+				switch (event.status) {
+					case 0xF0:
+					case 0xF7:
+						fprintf(data_stream, "SYSEX:\t");
+						for (size_t i = 0; i < event.size; ++i)
+							fprintf(data_stream, "%hhu ", event.sysex_data[i]);
+						break;
+					case 0xFF:
+						switch (event.meta_type) {
+							case MetaText:
+							case MetaCopyright:
+							case MetaTrackName:
+							case MetaInstrumentName:
+							case MetaLyrics:
+							case MetaMarker:
+							case MetaCuePoint:
+								fprintf(data_stream, "META:\t%s\n", event.meta_data.text);
+								break;
+						}
+				}
+			}
 		}
 
-		show_keyboard(notes, 128, stderr);
-		// usleep((useconds_t) MIDI_DELAY(parser));
-		parser->timestamp += parser->dtime;
+        #ifdef SHOW_KEYBOARD
+            show_keyboard(notes, 128, stderr);
+        #endif
+        #ifdef REAL_TIME
+            usleep((useconds_t) MIDI_DELAY(parser));
+        #endif
 	}
 
 	printf(
 		"midi_header\tmidi_event\tmidi_track\tmidi_parser\n"
-		"%lu\t%lu\t%lu\t%lu\n",
+		"%lu bytes\t\t%lu bytes\t%lu bytes\t%lu bytes\n",
 		sizeof(struct midi_header), sizeof(struct midi_event),
 		sizeof(struct midi_track), sizeof(struct midi_parser)
 	);
+
+	free(parser);
 }
 
 int main(int argc, char **argv)
@@ -65,11 +95,7 @@ int main(int argc, char **argv)
 		midi = fopen(argv[1], "rb");
 	}
 
-	struct midi_parser *parser = midi_parser_new(NULL, midi);
-
-	parse_midi(parser, midi);
-
-	free(parser);
+	parse(midi);
 
 	fclose(midi);
 	fclose(output);
